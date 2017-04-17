7775357f59a5ba7c0724d6a0362dc71b5514ccd8
Mark Sheppard <mark@orchardstreetpress.com>
2017-04-17 16:51:37 -0500

    some styling to header

--- assets/functions/enqueue-scripts.php
+++ assets/functions/enqueue-scripts.php
@@ -4,15 +4,21 @@ function site_scripts() {
 
     // Load What-Input files in footer
     wp_enqueue_script( 'what-input', get_template_directory_uri() . '/vendor/what-input/dist/what-input.min.js', array(), '', true );
-    
+
     // Adding Foundation scripts file in the footer
     wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/assets/js/foundation.js', array( 'jquery' ), '6.2.3', true );
-    
+
     // Adding scripts file in the footer
     wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), '', true );
-   
+
     // Register main stylesheet
     wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), '', 'all' );
+
+    // Add Google Fonts
+    wp_enqueue_style( 'osp-fonts', 'https://fonts.googleapis.com/css?family=Bitter:400,400i,700|Merriweather+Sans:700,800' );
+
+    // Add Font Awesome
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
     // Comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -1,19 +1,38 @@
-<!-- By default, this menu will use off-canvas for small
-	 and a topbar for medium-up -->
 
-<div class="top-bar" id="top-bar-menu">
-	<div class="top-bar-left float-left">
-		<ul class="menu">
-			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
-		</ul>
-	</div>
-	<div class="top-bar-right show-for-medium">
-		<?php joints_top_nav(); ?>	
-	</div>
-	<div class="top-bar-right float-right show-for-small-only">
-		<ul class="menu">
-			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
-			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
-		</ul>
-	</div>
-</div>
+	 <div class="responsive-nav-social-mobile" data-responsive-toggle="responsive-nav-social" data-hide-for="medium">
+	   <div class="responsive-nav-social-mobile-left">
+	     <ul class="menu">
+	       <li><a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+	       <li><a href="https://www.instagram.com/?hl=en"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+	       <li><a href="https://www.pinterest.com/"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+	       <li><a href="https://twitter.com/?lang=en"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+	     </ul>
+	   </div>
+	   <div class="responsive-nav-social-mobile-right">
+	     <button class="menu-icon" type="button" data-toggle="responsive-nav-social"></button>
+	   </div>
+	 </div>
+
+	 <div data-sticky-container>
+	   <div class="responsive-nav-social" id="responsive-nav-social" data-sticky data-options="marginTop:0;">
+	     <div class="row align-justify align-middle" id="responsive-menu">
+	       <div class="responsive-nav-social-left">
+	         <ul class="menu vertical medium-horizontal">
+	           <li><a href="#">Home</a></li>
+	           <li><a href="#">Order</a></li>
+	           <li><a href="#">Services</a></li>
+	           <li><a href="#">Contact</a></li>
+	           <li><a href="#">Blog</a></li>
+	         </ul>
+	       </div>
+	       <div class="responsive-nav-social-right hide-for-small-only">
+	         <ul class="menu">
+	           <li><a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+	           <li><a href="https://www.instagram.com/?hl=en"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+	           <li><a href="https://www.pinterest.com/"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+	           <li><a href="https://twitter.com/?lang=en"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+	         </ul>
+	       </div>
+	     </div>
+	   </div>
+	 </div>
