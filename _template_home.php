1220d2f662387db7dbffaa2f00de52387447249a
Mark Sheppard <mark@orchardstreetpress.com>
2017-04-18 15:51:28 -0500

    added testimonial section

--- _template_home.php
+++ _template_home.php
@@ -6,34 +6,116 @@ Template Name: Template Home
 
 <?php get_header(); ?>
 
-  <h3 class="services">Services We Offer</h3>
+<div class="testimonial-container">
+  <h2>What Motherfuckers Be Saying</h2>
 
-  <div class="services">
-    <h2 class="services-headline">Yeti Approved Features</h2>
-    <p class="services-subheadline subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit quaerat eos words and phrases.</p>
-    <div class="row">
-      <div class="small-12 medium-3 columns">
-        <i class="fa fa-snowflake-o" aria-hidden="true"></i>
-        <h4 class="services-title">Special Snowflake</h4>
-        <p class="services-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      </div>
-      <div class="small-12 medium-3 columns">
-        <i class="fa fa-angellist" aria-hidden="true"></i>
-        <h4 class="services-title">Friendly Forms </h4>
-        <p class="services-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      </div>
-      <div class="small-12 medium-3 columns">
-        <i class="fa fa-bullseye" aria-hidden="true"></i>
-        <h4 class="services-title">Special Snowflake</h4>
-        <p class="services-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      </div>
-      <div class="small-12 medium-3 columns">
-        <i class="fa fa-battery-full" aria-hidden="true"></i>
-        <h4 class="services-title">Lithium Ion</h4>
-        <p class="services-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      </div>
+  <div class="row">
+    <div class="orbit testimonial-slider-container" role="region" aria-label="testimonial-slider" data-orbit>
+      <ul class="orbit-container">
+        <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+        <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+
+        <!-- content slide 1 -->
+
+        <li class="is-active orbit-slide">
+          <div class="testimonial-slide row">
+            <div class="small-12 large-9 column">
+              <div class="row align-middle testimonial-slide-content">
+                <div class="small-12 medium-4 column hide-for-small-only profile-pic">
+                  <img src="http://placekitten.com/g/300/300">
+                </div>
+                <div class="small-12 medium-8 column testimonial-slide-text">
+                  <p class="testimonial-slide-quotation">Even though they've printed shirts for dogs, I still got love for them.</p>
+                  <div class="testimonial-slide-author-container">
+                    <div class="small-profile-pic show-for-small-only">
+                      <img src="http://placekitten.com/g/50/50">
+                    </div>
+                    <p class="testimonial-slide-author-info">Fleas Witherspoon<br><i class="subheader">Cat World Inc.</i></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+
+        <!--content slide 2 -->
+
+        <li class="orbit-slide">
+          <div class="testimonial-slide row">
+            <div class="small-12 large-9 column">
+              <div class="row align-middle testimonial-slide-content">
+                <div class="small-12 medium-4 column hide-for-small-only profile-pic">
+                  <img src="http://placekitten.com/g/300/300">
+                </div>
+                <div class="small-12 medium-8 column testimonial-slide-text">
+                  <p class="testimonial-slide-quotation">I asked for a shirt with the shape of Wisconsin on it and they totally delivered.</p>
+                  <div class="testimonial-slide-author-container">
+                    <div class="small-profile-pic show-for-small-only">
+                      <img src="http://placekitten.com/g/50/50">
+                    </div>
+                    <p class="testimonial-slide-author-info">Your Mom<br><i class="subheader">Your Face, Inc.</i></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+
+        <!-- content slide 3 -->
+
+        <li class="orbit-slide">
+          <div class="testimonial-slide row">
+            <div class="small-12 large-9 column">
+              <div class="row align-middle testimonial-slide-content">
+                <div class="small-12 medium-4 column hide-for-small-only profile-pic">
+                  <img src="http://placekitten.com/g/300/300">
+                </div>
+                <div class="small-12 medium-8 column testimonial-slide-text">
+                  <p class="testimonial-slide-quotation">The bathroom smells wonderful.</p>
+                  <div class="testimonial-slide-author-container">
+                    <div class="small-profile-pic show-for-small-only">
+                      <img src="http://placekitten.com/g/50/50">
+                    </div>
+                    <p class="testimonial-slide-author-info">Lance Bass<br><i class="subheader">N'Sync</i></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+
+        <!-- content slide 4 -->
+
+        <li class="orbit-slide">
+          <div class="testimonial-slide row">
+            <div class="small-12 large-9 column">
+              <div class="row align-middle testimonial-slide-content">
+                <div class="small-12 medium-4 column hide-for-small-only profile-pic">
+                  <img src="http://placekitten.com/g/300/300">
+                </div>
+                <div class="small-12 medium-8 column testimonial-slide-text">
+                  <p class="testimonial-slide-quotation">Really popular in Germany!</p>
+                  <div class="testimonial-slide-author-container">
+                    <div class="small-profile-pic show-for-small-only">
+                      <img src="http://placekitten.com/g/50/50">
+                    </div>
+                    <p class="testimonial-slide-author-info">David Hasselhoff<br><i class="subheader">Knightrider Inc.</i></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
+</div>
+<!-- slider close -->
+
+
+
+
+
 
 
 
--- assets/functions/sidebar.php
+++ assets/functions/sidebar.php
@@ -17,8 +17,8 @@ function joints_register_sidebars() {
 		'description' => __('The offcanvas sidebar.', 'jointswp'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget' => '</div>',
-		'before_title' => '<h4 class="widgettitle">',
-		'after_title' => '</h4>',
+		'before_title' => '<h5 class="widgettitle">',
+		'after_title' => '</h5>',
 	));
 
 	/*
