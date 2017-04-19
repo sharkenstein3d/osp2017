7303c96a872fe83088ccd457031d21283eaa5611
Mark Sheppard <mark@orchardstreetpress.com>
2017-04-19 14:34:14 -0500

    lots of shit added

--- _template_home.php
+++ _template_home.php
@@ -6,14 +6,51 @@ Template Name: Template Home
 
 <?php get_header(); ?>
 
+<h2 class="header-text-extra-space">Services We Offer</h2>
+<div class="row">
+  <div class="services-content-section">
+    <div class="services-content-section-img">
+      <img src="http://www.placebeard.it/800/800" alt="" />
+    </div>
+    <div class="services-content-section-block">
+      <h3 class="services-desc-header">Custom Screen Printing</h3>
+      <p class="services-desc">Band t-shirts, some shit about your local area, maybe some towels. What the fuck ever! Bring it on, and we'll print some fucking shit on it.</p>
+    </div>
+    <div class="services-content-section-block small-order-2 medium-order-1">
+      <h3 class="services-desc-header">Retail-Ready Finishing</h3>
+      <p class="services-desc">Need more than just shirts? How about some plastic for the landfill? Or some paper tags you'll just throw away? Say no more!</p>
+    </div>
+    <div class="services-content-section-img small-order-1 medium-order-2">
+      <img src="http://www.placebeard.it/g/800/800" alt="" />
+    </div>
+  </div>
+
+  <div class="services-content-section">
+    <div class="services-content-section-img">
+      <img src="http://www.placebeard.it/800x800" alt="" />
+    </div>
+    <div class="services-content-section-block">
+      <h3 class="services-desc-header">Professional Graphic Design</h3>
+      <p class="services-desc">No, your shit definitely isn't good enough. Trust us to do your dirty work.</p>
+    </div>
+    <div class="services-content-section-block small-order-2 medium-order-1">
+      <h3 class="services-desc-header">Custom Embroidery</h3>
+      <p class="services-desc">If you're too good and fancy for just regular-ass screen printing, you can always get some embroidery done!</p>
+    </div>
+    <div class="services-content-section-img small-order-1 medium-order-2">
+      <img src="http://www.placebeard.it/g/800x800" alt="" />
+    </div>
+  </div>
+</div>
+
 <div class="testimonial-container">
-  <h2>What Motherfuckers Be Saying</h2>
+  <h2 class="header-text-extra-space-white">What Motherfuckers Be Saying</h2>
 
   <div class="row">
     <div class="orbit testimonial-slider-container" role="region" aria-label="testimonial-slider" data-orbit>
       <ul class="orbit-container">
-        <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-        <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+        <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+        <button class="orbit-next"><span class="show-for-sr">Next Slide</span><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
 
         <!-- content slide 1 -->
 
@@ -45,15 +82,15 @@ Template Name: Template Home
             <div class="small-12 large-9 column">
               <div class="row align-middle testimonial-slide-content">
                 <div class="small-12 medium-4 column hide-for-small-only profile-pic">
-                  <img src="http://placekitten.com/g/300/300">
+                  <img src="http://placebeard.it/g/300x300">
                 </div>
                 <div class="small-12 medium-8 column testimonial-slide-text">
                   <p class="testimonial-slide-quotation">I asked for a shirt with the shape of Wisconsin on it and they totally delivered.</p>
                   <div class="testimonial-slide-author-container">
                     <div class="small-profile-pic show-for-small-only">
-                      <img src="http://placekitten.com/g/50/50">
+                      <img src="http://placebeard.it/g/50x50">
                     </div>
-                    <p class="testimonial-slide-author-info">Your Mom<br><i class="subheader">Your Face, Inc.</i></p>
+                    <p class="testimonial-slide-author-info">Some Motherfucker<br><i class="subheader">Your Face, Inc.</i></p>
                   </div>
                 </div>
               </div>
@@ -68,15 +105,15 @@ Template Name: Template Home
             <div class="small-12 large-9 column">
               <div class="row align-middle testimonial-slide-content">
                 <div class="small-12 medium-4 column hide-for-small-only profile-pic">
-                  <img src="http://placekitten.com/g/300/300">
+                  <img src="http://placebeard.it/300x300">
                 </div>
                 <div class="small-12 medium-8 column testimonial-slide-text">
                   <p class="testimonial-slide-quotation">The bathroom smells wonderful.</p>
                   <div class="testimonial-slide-author-container">
                     <div class="small-profile-pic show-for-small-only">
-                      <img src="http://placekitten.com/g/50/50">
+                      <img src="http://placebeard.it/50x50">
                     </div>
-                    <p class="testimonial-slide-author-info">Lance Bass<br><i class="subheader">N'Sync</i></p>
+                    <p class="testimonial-slide-author-info">Some Other Motherfucker<br><i class="subheader">Taco Hut, Inc.</i></p>
                   </div>
                 </div>
               </div>
@@ -91,15 +128,15 @@ Template Name: Template Home
             <div class="small-12 large-9 column">
               <div class="row align-middle testimonial-slide-content">
                 <div class="small-12 medium-4 column hide-for-small-only profile-pic">
-                  <img src="http://placekitten.com/g/300/300">
+                  <img src="http://placebeard.it/g/300/300">
                 </div>
                 <div class="small-12 medium-8 column testimonial-slide-text">
                   <p class="testimonial-slide-quotation">Really popular in Germany!</p>
                   <div class="testimonial-slide-author-container">
                     <div class="small-profile-pic show-for-small-only">
-                      <img src="http://placekitten.com/g/50/50">
+                      <img src="http://placebeard.it/g/50/50">
                     </div>
-                    <p class="testimonial-slide-author-info">David Hasselhoff<br><i class="subheader">Knightrider Inc.</i></p>
+                    <p class="testimonial-slide-author-info">David Hasselhoff<br><i class="subheader">Knight Rider, Inc.</i></p>
                   </div>
                 </div>
               </div>
--- footer.php
+++ footer.php
@@ -1,14 +1,5 @@
 				<footer class="footer" role="contentinfo">
-					<div id="inner-footer" class="row">
-						<div class="large-12 medium-12 columns">
-							<nav role="navigation">
-	    						<?php joints_footer_links(); ?>
-	    					</nav>
-	    				</div>
-						<div class="large-12 medium-12 columns">
-							<p class="source-org copyright">&copy; 2008-<?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All Rights Reserved.</p> 
-						</div>
-					</div> <!-- end #inner-footer -->
+					<p class="source-org copyright">&copy; 2008-<?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All Rights Reserved.</p>
 				</footer> <!-- end .footer -->
 			</div>  <!-- end .main-content -->
 		</div> <!-- end .off-canvas-wrapper -->
