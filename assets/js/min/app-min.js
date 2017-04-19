44c65dfadad54ac9c0c451af9e75d4e52b69732f
Mark Sheppard <mark@orchardstreetpress.com>
2017-04-19 11:19:11 -0500

    fucked up some shit, fixed some shit, fuck. shit.

---
+++ assets/js/min/app-min.js
@@ -0,0 +1,1 @@
+function initMap(){var a={lat:42.968308,lng:-87.870756};map=new google.maps.Map(document.getElementById("osp-map"),{center:a,zoom:14,draggable:!1,scrollwheel:!1});var e=new google.maps.Marker({position:a,map:map,title:"Orchard Street Press",icon:"/img/map-icon.png"})}var gMapAPIKey="AIzaSyBdjHO5IJwQIIdbDLJllcYpLHRB_5cAA98",map;
