ff0048bc92012be110c474c758d3401bb8a64afe
Mark Sheppard <mark@orchardstreetpress.com>
2017-05-09 17:00:20 -0500

    hurry up and wait

--- order-test.php
+++ order-test.php
@@ -7,7 +7,7 @@ Template Name: Order2
 
 <h2 class="services">Quote Submission Form</h2>
 <p class="section-description-gray">
-	Please complete this form and we will contact you as soon as possible. Note that, for now, this form is a quote generator and not actually an order form. Should you need assistance, please feel free to contact us via email or phone to discuss your order.
+	Please complete this form and we will contact you as soon as possible. Note that, for now, this form is a quote generator and not actually an order form. Should you need assistance, please feel free to <a href="/home/#contact">contact us via email or phone</a> to discuss your order.
 </p>
 
 	<?php if (function_exists('iphorm')) echo iphorm(1); ?>
