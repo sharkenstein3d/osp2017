1f83a7ce6a1200c0fb4b1057dd6500ec7443d7cf
Mark Sheppard <mark@orchardstreetpress.com>
2017-06-15 15:28:58 -0500

    fusdoorserogher

--- assets/functions/enqueue-scripts.php
+++ assets/functions/enqueue-scripts.php
@@ -1,34 +1,26 @@
 <?php
 function site_scripts() {
   global $wp_styles; // Call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
-
-    // Load What-Input files in footer
-    wp_enqueue_script( 'what-input', get_template_directory_uri() . '/vendor/what-input/dist/what-input.min.js', array(), '', true );
-
-    // Adding Foundation scripts file in the footer
-    wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/assets/js/foundation.js', array( 'jquery' ), '6.2.3', true );
-
-    // Add Google Maps
-    wp_enqueue_script( 'google-maps', 'https://maps.googleapis.com/maps/api/js');
-
-    // Adding scripts file in the footer
-    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.min.js', array( 'jquery' ), '', true );
-
-    // Adding scripts file in the footer
-    wp_enqueue_script( 'fancybox-js', 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js', array( 'jquery' ), '', true );
-
-    // Register main stylesheet
+  
     wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), '', 'all' );
 
-    // Add Google Fonts
     wp_enqueue_style( 'osp-fonts', 'https://fonts.googleapis.com/css?family=Bitter:400,400i,700|Palanquin+Dark:600,700' );
 
-    // FancyBox
     wp_enqueue_style( 'fancybox-css', 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css', array(), '', 'all' );
 
-
-    // Add Font Awesome
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+    wp_enqueue_script( 'what-input', get_template_directory_uri() . '/vendor/what-input/dist/what-input.min.js', array(), '', true );
+
+    wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/assets/js/foundation.js', array( 'jquery' ), '6.2.3', true );
+
+    wp_enqueue_script( 'google-maps', 'https://maps.googleapis.com/maps/api/js');
+
+    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.min.js', array( 'jquery' ), '', true );
+  
+    wp_enqueue_script( 'fancybox-js', 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js', array( 'jquery' ), '', true );
+  
+  
 
     // Comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
--- order-test.php
+++ order-test.php
@@ -7,10 +7,180 @@ Template Name: Order2
 
 <h2 class="services">Quote Submission Form</h2>
 <p class="section-description-gray">
-	Please complete this form and we will contact you as soon as possible. Note that, for now, this form is a quote generator and not actually an order form. Should you need assistance, please feel free to <a href="/home/#contact">contact us via email or phone</a> to discuss your order.
+	Please <strong>complete this form</strong> this form and we will contact you as soon as possible. Should you need assistance, please feel free to <a href="/home/#contact">contact us via email or phone</a> to discuss your order.
 </p>
 
-	<?php if (function_exists('iphorm')) echo iphorm(1); ?>
+<form>
+ 
+  <div class="row align-spaced">
+   
+    <div class="small-11 medium-5 columns customer">
+      <label class="order-form">First Name
+        <input type="text">
+      </label>
+       <label class="order-form">Last Name
+        <input type="text">
+      <label class="order-form">Company
+        <input type="text">
+      </label>
+      </label>
+      <label class="order-form">Phone Number
+        <input type="tel">
+      </label>
+      <label class="order-form">Email Address
+        <input type="email">
+      </label>
+      <label class="order-form">URL
+        <input type="email">
+      </label>
+    </div>
+    
+    <div class="small-11 medium-5 columns customer">
+      <label class="order-form">Address 1
+        <input type="text">
+      </label>
+      <label class="order-form">Address 2
+        <input type="text">
+      </label>
+      <label class="order-form">City
+        <input type="text">
+      </label>
+      <label class="order-form">State
+        <select>
+          <option selected disabled>Please select one</option>
+          <option value="AL">Alabama</option>
+          <option value="AK">Alaska</option>
+          <option value="AZ">Arizona</option>
+          <option value="AR">Arkansas</option>
+          <option value="CA">California</option>
+          <option value="CO">Colorado</option>
+          <option value="CT">Connecticut</option>
+          <option value="DE">Delaware</option>
+          <option value="DC">District Of Columbia</option>
+          <option value="FL">Florida</option>
+          <option value="GA">Georgia</option>
+          <option value="HI">Hawaii</option>
+          <option value="ID">Idaho</option>
+          <option value="IL">Illinois</option>
+          <option value="IN">Indiana</option>
+          <option value="IA">Iowa</option>
+          <option value="KS">Kansas</option>
+          <option value="KY">Kentucky</option>
+          <option value="LA">Louisiana</option>
+          <option value="ME">Maine</option>
+          <option value="MD">Maryland</option>
+          <option value="MA">Massachusetts</option>
+          <option value="MI">Michigan</option>
+          <option value="MN">Minnesota</option>
+          <option value="MS">Mississippi</option>
+          <option value="MO">Missouri</option>
+          <option value="MT">Montana</option>
+          <option value="NE">Nebraska</option>
+          <option value="NV">Nevada</option>
+          <option value="NH">New Hampshire</option>
+          <option value="NJ">New Jersey</option>
+          <option value="NM">New Mexico</option>
+          <option value="NY">New York</option>
+          <option value="NC">North Carolina</option>
+          <option value="ND">North Dakota</option>
+          <option value="OH">Ohio</option>
+          <option value="OK">Oklahoma</option>
+          <option value="OR">Oregon</option>
+          <option value="PA">Pennsylvania</option>
+          <option value="RI">Rhode Island</option>
+          <option value="SC">South Carolina</option>
+          <option value="SD">South Dakota</option>
+          <option value="TN">Tennessee</option>
+          <option value="TX">Texas</option>
+          <option value="UT">Utah</option>
+          <option value="VT">Vermont</option>
+          <option value="VA">Virginia</option>
+          <option value="WA">Washington</option>
+          <option value="WV">West Virginia</option>
+          <option value="WI">Wisconsin</option>
+          <option value="WY">Wyoming</option>
+          <option value="Outside of US">Other / Outside of US</option>
+      </select>			
+      </label>
+      <label class="order-form">Zipcode
+        <input type="text">
+      </label>
+    </div>
+
+    <h2 class="order-sections">Tell Us About Your Order</h2>
+               
+    <div class="small-11 columns order-info">
+     
+      <h3 class="design-sections">Design 1</h3>
+      <h4 class="design-sections-inner">Blank 1</h4>
+       <a class="addsection button secondary">Add blank</a>
+       
+      <hr>
+      
+      <div id="sections">      
+        <h3 class="design-sections">Prints</h3>
+        <h4 class="design-sections-inner">Print 1</h4>
+
+        <div class="row align-spaced">
+          <div class="column small-11 medium-5">
+            <label class="order-form">Print Location
+              <select>
+                <option val="selection" selected disabled>Please select one</option>
+                <option val="front">Front</option>
+                <option val="left-chest">Left chest</option>
+                <option val="pocket">Pocket</option>
+                <option val="front-top">Front top</option>
+                <option val="front-bottom">Front bottom</option>
+                <option val="right-side">Right side</option>
+                <option val="right-sleeve">Right sleeve</option>
+                <option val="left-side">Left side</option>
+                <option val="left-sleeve">Left sleeve</option>
+                <option val="back">Back</option>
+                <option val="back-top">Back top</option>
+                <option val="back-bottom">Back bottom</option>
+                <option val="tag">Tag</option>
+              </select>			
+            </label>
+          </div>
+
+          <div class="column small-11 medium-5">
+            <label class="order-form">Design Name
+            <input type="text">
+            </label>
+            <label for="exampleFileUpload" class="button alert">Upload File</label>
+            <input type="file" id="exampleFileUpload" class="show-for-sr">
+          </div>
+
+          <div class="column small-11 medium-10">
+            <label class="order-form notes">
+              Notes:
+              <textarea rows="4"></textarea>
+            </label>
+          </div>
+        </div>
+      
+      
+      
+        <a class="button secondary">Add print</a>
+      </div>
+      
+      <hr>
+      
+      <h3 class="design-sections">Finishing Options</h3>
+      
+        <div class="row align-center">
+          <fieldset>
+            <input id="checkbox1" type="checkbox"><label for="checkbox1" class="order-form">Folding</label>
+            <input id="checkbox2" type="checkbox"><label for="checkbox2" class="order-form">Poly bagging</label>
+            <input id="checkbox3" type="checkbox"><label for="checkbox3" class="order-form">Hang tagging</label>
+            <input id="checkbox4" type="checkbox"><label for="checkbox4" class="order-form">Size stickers</label>
+          </fieldset>
+        </div>
+      
+    </div>
+  </div><!--  container-->
+  
+</form>
 
 
 
